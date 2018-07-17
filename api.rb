75af8b45e825e6d4f8712587d8289049bf0cc27e
zhouzhenye <zhouzhenye530@gmail.com>
2018-07-17 14:40:05 +0900

    7/17 課題提出

--- api.rb
+++ api.rb
@@ -6,15 +6,20 @@ require 'json'
 
 get '/show' do
   # Rubyのハッシュを作成、diaryという名前を付ける
-  diary = {
-      id: 1,
-      title: "今日の日記",
-      content: "今日はGETメソッドを学んだ"
-  }
+  #diary = {
+  #    id: 1,
+  #    title: "今日の日記",
+  #    content: "今日はGETメソッドを学んだ"
+  #}
 
   # diaryをJSONテキストに変換して返す
   # （Rubyでは、最終行はメソッドの返り値(return)）
-  diary.to_json
+  param1 = params['param1']
+  param2 = params['param2']
+
+  '1番目：' + param1 +',2番目' + param2
+  
+ # diary.to_json
 end
 
 # POST /edit
@@ -23,7 +28,7 @@ end
 
 post '/edit' do
   # リクエストボディを読み込む
-  body = request.body.read
+  body = request.body.read.force_encoding("utf-8")
 
   if body == ''
     # HTTPのステータスコード「400」（Bad Request）を返す
